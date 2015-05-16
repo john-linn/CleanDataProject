2d84bd5bf04f037d732469c84b16c53e72a79e70
john-linn <jlinn@linndom.net>
2015-05-16 15:06:52 -0400

    Initial R script check-in
    Copy R code from development directory

---
+++ run_analysis.R
@@ -0,0 +1,69 @@
+library(dplyr)
+
+analyze <- function () {
+  # Part 1
+  # Read partitioned dataset components into data frames
+  test_subj <- read.table("UCI HAR Dataset/test/subject_test.txt")
+  test_x <- read.table("UCI HAR Dataset/test/X_test.txt")
+  test_y <- read.table("UCI HAR Dataset/test/y_test.txt")
+  train_subj <- read.table("UCI HAR Dataset/train/subject_train.txt")
+  train_x <- read.table("UCI HAR Dataset/train/X_train.txt")
+  train_y <- read.table("UCI HAR Dataset/train/y_train.txt")
+
+  # Consolidate datasets
+  comb_subj <- rbind(test_subj, train_subj)
+  comb_x <- rbind(test_x, train_x)
+  comb_y <- rbind(test_y, train_y)
+
+  # Clean up now-superfluous working objects
+  rm(test_subj, test_x, test_y, train_subj, train_x, train_y)
+
+  # Part 2
+  # Select only the computed means and standard deviations for 17 variables
+  # Features.txt identifies columns corresponding to variables
+
+  # Ingest variable names
+  varnames <- read.table("UCI HAR Dataset/features.txt", 
+                       stringsAsFactors = FALSE,check.names=FALSE)
+  names_mean <- grep(pattern="-mean()",varnames[,2], fixed=TRUE)
+  names_std <- grep(pattern="-std()", varnames[,2], fixed=TRUE)
+
+  comb_subsx <- select(comb_x, sort(c(names_mean,names_std)))
+
+  # Prepend subject IDs as first table column
+  comb_subsx <- cbind(comb_subj,comb_subsx)
+  names(comb_subsx)[1] <- "Subject"
+
+  # Part 3
+  # Ingest activity label names
+  actnames <- read.table("UCI HAR Dataset/activity_labels.txt",
+                         stringsAsFactors = FALSE)
+  # Create vector of names
+  Activity <- actnames[comb_y[[1]],2]
+  comb_subsx <- cbind(Activity,comb_subsx)
+
+  # Clean up temporary objects now incorporated into comb_subsx
+  rm(comb_x, comb_y, comb_subj, Activity, actnames)
+
+  # Part 4
+  # Derive numeric-form column names
+  varncols <- mutate(varnames, sname=paste("V", 
+                      as.character(varnames[,1]), sep=""))
+  # Extract indices into varncols for each "Vnn" column name in comb_subsx,
+  # excepting comb_subsx's first (Subject) and 2nd (Activity) column
+  nindex <- match(names(comb_subsx[-c(1:2)]),varncols[,3])
+  # Look up corresponding column names
+  namevec <- varnames[nindex,2]
+  # Apply names ingested from features.txt to comb_subsx columns
+  names(comb_subsx)[-c(1:2)] <- namevec
+  rm(nindex,namevec)
+
+  # Part 5
+  grouped_both <- group_by(comb_subsx,Activity,Subject)
+  # Valued SO pointers to recent dplyr function (with British spelling only)
+  grpout <- summarise_each(grouped_both,"mean")
+  write.table(grpout,"ucidat.txt",row.names=FALSE)
+}
+
+analyze()
+
